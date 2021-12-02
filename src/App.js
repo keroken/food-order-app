dcf8f30a8968be5a43f8a850bddb12a66b84b961
keroken <kenichi.shinagawa@gmail.com>
2021-12-02 23:46:35 +0900

    add Meals to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-    </div>
+      <main>
+        <Meals />
+      </main>
+    </>
   );
 }
 
